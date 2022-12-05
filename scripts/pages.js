0b59be66bd6e5c72dd2054127e410a6c522df7d9
ShafikovAlexander <shafikov.alexander@gmail.com>
2022-12-05 14:11:35 +0300

    feat:add other tasks for lab-4

---
+++ scripts/pages.js
@@ -0,0 +1,21 @@
+const paths = Object.freeze({
+    Page2: { file: 'Lab-1/page_2.html', link: 'page2'},
+    Page3: { file: 'Lab-1/page_3.html', link: 'page3'},
+    Page4: { file: 'Lab-1/page_4.html', link: 'page4'}
+});
+
+(() => {
+    document.addEventListener('DOMContentLoaded', () => {
+        const navigationLinks = document.querySelectorAll('.header__link_normal');
+        switch (true) {
+            case document.location.pathname.endsWith(paths.Page2.file):
+                [...navigationLinks].find(link => link.dataset.link === paths.Page2.link).classList.add('header__link_normal-active');
+                break;
+            case document.location.pathname.endsWith(paths.Page3.file):
+                [...navigationLinks].find(link => link.dataset.link === paths.Page3.link).classList.add('header__link_normal-active');
+                break;
+            case document.location.pathname.endsWith(paths.Page4.file):
+                [...navigationLinks].find(link => link.dataset.link === paths.Page4.link).classList.add('header__link_normal-active');
+        }
+    })
+})();
