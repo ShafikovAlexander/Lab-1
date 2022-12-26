e6d01c0017c3bf0532c60b14f5e11b23f1dcc7b5
ShafikovAlexander <shafikov.alexander@gmail.com>
2022-12-26 15:37:01 +0300

    fix: fix null problem while reading comments

--- scripts/fetch.js
+++ scripts/fetch.js
@@ -1,5 +1,5 @@
 const fetchComments = async (page = 1) => {
-    const response = await fetch(`https://jsonplaceholder.typicode.com/posts/${page}/comments`);
+    const response = await fetch(`https://jsonplaceholder.typicode.com/posts/1/comments`);
     return await response.json();
 }
 
